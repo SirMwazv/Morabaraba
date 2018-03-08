9570a491e51c9953f3bbd4abc107cfece59da3fb
SirMwazv <mwazvitam@gmail.com>
2018-03-09 00:37:57 +0200

    Initial commit

---
+++ ProgramData.fs
@@ -0,0 +1,30 @@
+﻿module ProgramData
+
+type Position =
+| A1 | A4 | A7
+| B2 | B4 | B6
+| C3 | C4 | C5
+| D1 | D2 | D3 | D5 | D6 | D7
+| E3 | E4 | E5
+| F2 | F4 | F6
+| G1 | G4 | G7
+
+type PlayerColor = 
+| Dark
+| Light
+
+type Cow =
+| Onboard of PlayerColor * Position 
+| Flying of PlayerColor * Position
+| NotOnBoard of PlayerColor
+| Dead of PlayerColor
+
+type Player = {
+    Alias : string
+    Color : PlayerColor
+    Cows : Cow list
+    MyMills : (Position*Position*Position) list
+}
+
+
+
